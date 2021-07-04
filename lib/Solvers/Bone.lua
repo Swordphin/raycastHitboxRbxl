e4a21e80db90bf69d6a8bd16446b755f81e09ab9
Jackie <jackie_yeung@protonmail.com>
2021-07-03 21:59:11 -0400

    Fix to Bone's WorldSpace solver
    The bone solver did not account for rotation of the model and only moved in world space. It did not take into consideration the relative rotation. This commit is a fix to that problem.

--- lib/Solvers/Bone.lua
+++ lib/Solvers/Bone.lua
@@ -12,7 +12,8 @@ function solver:Solve(point: {[string]: any}): (Vector3, Vector3)
 	--- Translate localized bone positions to world space values
 	local originBone: Bone = point.Instances[1]
 	local vector: Vector3 = point.Instances[2]
-	local pointToWorldSpace: Vector3 = originBone.TransformedWorldCFrame.Position + vector
+	local worldCFrame: CFrame = originBone.TransformedWorldCFrame
+	local pointToWorldSpace: Vector3 = worldCFrame.Position + worldCFrame:VectorToWorldSpace(vector)
 
 	--- If LastPosition is nil (caused by if the hitbox was stopped previously), rewrite its value to the current point position
 	if not point.LastPosition then
